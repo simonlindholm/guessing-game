5d159e097b5b99c326ec86854332312f3a12083d
Simon Lindholm <simon.lindholm10@gmail.com>
2012-05-03 11:47:51 +0200

    API

---
+++ guess.h
@@ -0,0 +1,55 @@
+
+namespace GuessAPI {
+
+struct Card {
+	int rank, suit;
+	int value() const {
+		return rank*4 + suit;
+	}
+};
+
+struct Action {
+	enum {
+		TYPE_GUESS,
+		TYPE_PLAY
+	} type;
+	int targetPlayer;
+	union {
+		struct {
+			int ind;
+			Card guess;
+		} gdata;
+		struct {
+			Card given;
+		} pdata;
+	};
+};
+
+struct ActionResult {
+	Action action;
+	union {
+		struct {
+			bool correct;
+		} gdata;
+		struct {
+			Card drawn;
+			int ind; // returned by placedCard
+		} pdata;
+	};
+};
+
+class Bot {
+	public:
+		virtual ~Bot() = 0;
+		virtual const char* name() const = 0;
+		virtual Action move() = 0;
+		virtual int placedCard(int byPlayer, Card card) = 0;
+		virtual void madeMove(int player, const ActionResult& move) = 0;
+};
+
+template<class B>
+Bot* makeBot(int n, Card hidden[], Card hand[]) {
+	return new B(n, hidden, hand);
+}
+
+}
