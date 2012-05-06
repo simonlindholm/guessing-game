d0a3e5c87fa431b2ada9aa98a420df249fac05b7
Simon Lindholm <simon.lindholm10@gmail.com>
2012-05-06 15:12:27 +0200

    API things

--- api.h
+++ api.h
@@ -7,7 +7,18 @@ struct Card {
 	int value() const {
 		return rank*4 + suit;
 	}
+	bool isValid() const {
+		return 0 <= rank && rank < 13 &&
+			0 <= suit && suit < 4;
+	}
 };
+
+bool operator==(const Card& a, const Card& b) {
+	return a.value() == b.value();
+}
+bool operator<(const Card& a, const Card& b) {
+	return a.value() < b.value();
+}
 
 struct Action {
 	enum {
@@ -33,7 +44,7 @@ struct ActionResult {
 			bool correct;
 		} gdata;
 		struct {
-			Card drawn;
+			Card* drawn; // null, or a temporary copy
 			int ind; // returned by placedCard
 		} pdata;
 	};
@@ -43,10 +54,10 @@ class Bot {
 	public:
 		virtual ~Bot() = 0;
 		virtual const char* winPhrase() const = 0;
-		virtual void start(int nplayers, int turn, Card hidden[], Card hand[]) = 0; // hidden, hand temporary
+		virtual void start(int nplayers, int turn, Card hidden[], Card hand[]) = 0; // arrays always contain current state
 		virtual const char* name() const = 0;
 		virtual Action move() = 0;
-		virtual int placedCard(int byPlayer, Card card) = 0;
+		virtual int placedCard(int byPlayer, Card card) = 0; // 0-3, or -1; invalid value = random
 		virtual void madeMove(int player, const ActionResult& move) = 0;
 };
 
